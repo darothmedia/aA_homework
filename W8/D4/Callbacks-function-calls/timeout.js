a3ab0035ab69a612b786f9029c952cfad94d1857
darothmedia <87622162+darothmedia@users.noreply.github.com>
2021-10-20 21:23:45 -0700

    hammertime

--- W8/D4/Callbacks-function-calls/timeout.js
+++ W8/D4/Callbacks-function-calls/timeout.js
@@ -1,3 +1,11 @@
 setTimeout(function() {alert('HAMMERTIME');}, 5000 );
 
+setInterval(function() {alert('HAMMERTIME');}, 5000);
+
+
+function hammerTime(time) {
+  setTimeout(alert, 5000, `${time} is HAMMERTIME!`);
+}
+
+hammerTime('2PM')
 
