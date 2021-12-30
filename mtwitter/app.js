a6faa25c502313783d11e384fe7c5feaca251bb4
darothmedia <87622162+darothmedia@users.noreply.github.com>
2021-12-30 16:59:56 -0500

    progress on node

--- mtwitter/app.js
+++ mtwitter/app.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to mongoDB"))
+  .catch(err => console.log(err))
 
 app.get("/", (req, res) => {
+  debugger
   res.send("Hello a/A!");
 });
 
