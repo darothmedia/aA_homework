68f31d8ff1cf2527ff596a4318d1fe27bfda0c51
darothmedia <87622162+darothmedia@users.noreply.github.com>
2022-01-06 09:52:40 -0800

    progress on user model

--- mtwitter/routes/api/users.js
+++ mtwitter/routes/api/users.js
@@ -1,8 +1,35 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require('bcryptjs')
+const User = require('../../models/User')
 
 router.get("/test", (req, res) => {
   res.json({ msg: "This is the user route" });
 });
+
+router.post('/register', (req, res) => {
+  User.findOne({username: req.body.username})
+    .then(user => {
+      if (user) {
+        return res.status(400).json({username: "This username has already been taken"})
+      } else {
+        const newUser = new User({
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password
+        })
+
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if (err) throw err;
+            newUser.password = hash;
+            newUser.save()
+              .then(user => res.json(user))
+              .catch(err => console.log(err));
+          })
+        })
+      }
+    })
+})
 
 module.exports = router;
